bd8539b7c785cad72d6b4087d5537a470f9f7b0d
Nikolay Dobrovolskiy <n.dobrovolskiy@samolet.ru>
2023-12-04 10:53:57 +0300

    New: Make basic project's setup

---
+++ src/Tests/TestApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = @"c:\\Work\Projects\github\VisualizeIt\src\Tests\TestApp\..input";
+            string inputPath = Path.Combine(input, "SimpleClass.cs");
+            System.Console.WriteLine(inputPath + " - " + File.Exists(inputPath));
+
+        }
+    }
+}
